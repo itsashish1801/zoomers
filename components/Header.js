8a71cbeaaa980d173885e107ae312c41bf9b7b49
Ashish Sharma <itsashishsharma1801@gmail.com>
2022-08-21 19:27:56 +0530

    Create Header component.

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,40 @@
-import clsx from 'clsx';
+import Link from 'next/link';
+import Container from './Container';
+import Logo from '../assets/zoomers-dark.png';
+import Image from 'next/image';
+import { ButtonLink } from '../components/Button';
 
-const Container = ({ className, ...props }) => {
+const Header = () => {
   return (
-    <div
-      className={clsx('mx-auto max-w-7xl px-4 sm:px-6 lg:px-8', className)}
-      {...props}
-    />
+    <header className='py-10'>
+      <Container>
+        <nav className='relative z-50 text-sm'>
+          <ul className='flex items-center justify-between'>
+            <li>
+              <Link href='/'>
+                <a>
+                  <span className='sr-only'>Home</span>
+                  <Image src={Logo} alt='Cerify logo' height={60} width={180} />
+                </a>
+              </Link>
+            </li>
+            <div className='flex space-x-5'>
+              <li>
+                <ButtonLink href={'/'} variant='outline' className=''>
+                  Create account
+                </ButtonLink>
+              </li>
+              <li>
+                <ButtonLink href={'/'} className=''>
+                  Sign in
+                </ButtonLink>
+              </li>
+            </div>
+          </ul>
+        </nav>
+      </Container>
+    </header>
   );
 };
 
-export default Container;
+export default Header;
